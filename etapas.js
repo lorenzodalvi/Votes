44f440216bb1c71ee9d7a026d55baf546a2d6f4c
Lorenzo Dalvi <lorenzodalvi2410@gmail.com>
2022-07-17 22:13:21 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        título: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano',
+                partido: 'ABC',
+                fotos:[
+                    {url:'imgs/baixados (1).jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano',
+                partido: 'DEFG',
+                fotos:[
+                    {url: 'imgs/baixados.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+        ]
+    },
+    {
+            título: 'PREFEITO',
+            numeros: 2,
+            candidatos: [
+                {
+                    numero: '99',
+                    nome: 'Ciclano',
+                    partido: 'ABC',
+                    vice: 'Zul',
+                    fotos:[
+                        {url:'imgs/baixados (1).jpg', legenda: 'Prefeito'},
+                        {url:'imgs/baixados.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ]
+                    
+                },
+                {
+                    numero: '84',
+                    nome: 'Filtrano',
+                    partido: 'ZAP',
+                    vice: 'Kkkk',
+                    fotos:[
+                        {url:'imgs/baixados (2)', legenda: 'Prefeito'},
+                        {url:'imgs/baixados.jpg', legenda: 'Vice-Prefeito', small: true}
+                    ] 
+                }
+            ]
+    }
+];
